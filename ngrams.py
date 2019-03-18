5428284c0a62fe63dcfcdeefb1b12ca511bed9db
George Albertson <gdea73@gmail.com>
2019-03-18 00:36:19 -0500

    implemented ngram generation for word bigrams and trigrams (at least); included sample data

--- ngrams.py
+++ ngrams.py
@@ -19,7 +19,93 @@ def print_usage():
     print('    note: the [min, max] length range is inclusive.')
     print('filenames: optional list of files to read in lieu of stdin.')
 
-def build_ngram_model(stdin):
+# selects one of the possible suffixes to a given ngram mapping
+# weights them according to their relative frequency in the corpus
+def weighted_select(ngram_mapping):
+    # TODO write this
+    total_count = 0
+    delimiters = list()
+    delimiters.append(0)
+    for count in ngram_mapping.values():
+        total_count += count
+        # maintain running sum of suffix frequency
+        delimiters.append(delimiters[-1] + count)
+    random_index = random.randrange(total_count)
+    # index into the running sum to determine the suffix for selection
+    suffix_index = 1
+    # print('delimiters: {0}'.format(delimiters))
+    while delimiters[suffix_index] < random_index:
+        suffix_index += 1
+    # compensate for 1-indexed array of delimiters
+    suffix_index -= 1
+    # print('suffix_index: {0}'.format(suffix_index))
+    # the keys are most likely returned in insertion order, but the choice should still be random
+    suffixes = list(ngram_mapping.keys())
+    # print('suffixes: {0}'.format(suffixes))
+    return suffixes[suffix_index].split(' ')
+
+class NgramModel:
+    def __init__(self, n):
+        self.prefixes = dict()
+        self.n = n
+    
+    def build(self, corpus):
+        for i in range(len(corpus) - (self.n - 1)):
+            word = corpus[i]
+            # build the suffix
+            suffix = ''
+            for j in range(1, self.n):
+                # words in the suffix are joined with spaces for use as the dictionary key
+                suffix += corpus[i + j] + ' '
+            suffix = suffix.rstrip(' ')
+
+            if not word in self.prefixes:
+                # the first word of this ngram has not been seen before; register the prefix
+                # print('new prefix: {0}'.format(word))
+                self.prefixes[word] = dict()
+            # count the suffix occurrence
+            if not suffix in self.prefixes[word]:
+                # register the suffix within the mapping, so its count can be incremented to 1 below
+                self.prefixes[word][suffix] = 0
+            # known suffix; increment its occurence count
+            self.prefixes[word][suffix] += 1
+
+    # returns an ngram beginning with the given prefix in the form of a list of words
+    def generate_ngram(self, prefix):
+        # print('generating {0}gram for prefix: {1}'.format(self.n, prefix))
+        words = list()
+        words.append(prefix)
+        words += weighted_select(self.prefixes[prefix])
+        # print('ngram: {0}'.format(' '.join(words)))
+        return words
+
+
+def generate_ngram_iteration(ngram_model, n, start_token, end_token, min_length, max_length):
+    done_generating = False
+    generated_words = list()
+    ngram_prefix = start_token
+    generated_words.append(start_token)
+
+    while not done_generating:
+        ngram = ngram_model.generate_ngram(ngram_prefix)
+        if end_token in ngram:
+            if len(generated_words) >= min_length:
+                stop_index = ngram.index(end_token)
+                if stop_index == 0:
+                    print('warning: ngram generated beginning with an end token')
+                # add the ngram up until (but not including) the end token to complete the iteration
+                for i in range(1, stop_index):
+                    generated_words.append(ngram[i])
+                break
+            # retry ngram generation if an end token is reached before the minimum length
+        else:
+            generated_words += ngram[1:]
+            if len(generated_words) + n > max_length:
+                break
+        # use the last word generated as the prefix for the next ngram
+        ngram_prefix = ngram[n - 1] 
+        
+    return generated_words
 
 def main():
     if len(sys.argv) < 7:
@@ -70,10 +156,15 @@ def main():
             print(generated_text)
             print(ITERATION_SEPARATOR)
     else:
-        ngram_model = build_ngram_model(stdin)
-        generated_text = generate_text(ngram_model)
-        for line in generated_text:
-            print(line)
+        ngram_model = NgramModel(n)
+        ngram_model.build(stdin.split(' '))
+        for i in range(iterations):
+            generated_words = list()
+            # generate_ngram_iteration will ensure it meets the minimum length
+            generated_words = generate_ngram_iteration(ngram_model, n, start_token, end_token,
+                min_length, max_length)
+            print(' '.join(generated_words))
+            print(ITERATION_SEPARATOR)
 
 if __name__ == '__main__':
     main()
