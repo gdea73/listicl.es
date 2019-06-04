a7c7e30588c72b609f7da89f6df4075292a63430
alber461 <alber461@umn.edu>
2019-06-04 00:14:09 -0500

    reconfigured express to use HTTPS

--- web/app.js
+++ web/app.js
@@ -15,6 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+app.use(require('helmet')());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
